6874a7dea9bfbd572db8ff093ee226978740ba5c
Konstantin Neradovsky <konstantin.neradovskiy@open.ru>
2016-08-16 16:23:20 +0300

    Task added, gulp file copied from vsts-task repo

---
+++ Tasks/SpecFlowFeatures/index.ts
@@ -0,0 +1,21 @@
+/// <reference path="../../typings/index.d.ts" />
+import tl = require('vsts-task-lib/task');
+import trm = require('vsts-task-lib/toolrunner');
+
+async function run() {
+    try {
+        let rc1: number = await tl.tool('echo').arg(tl.getInput('samplestring', true)).exec();
+        
+        let rc2: number = -1;
+        if (tl.getBoolInput('samplebool')) {
+            rc2 = await tl.tool('echo').arg(tl.getInput('samplepathinput', true)).exec();    
+        }
+        
+        console.log('Task done! ' + rc1 + ',' + rc2);
+    }
+    catch (err) {
+        tl.setResult(tl.TaskResult.Failed, err.message);
+    }
+}
+
+run();
